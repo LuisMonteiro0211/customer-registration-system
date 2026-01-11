a434cb741ef0da4bf5c23387365d8958d64a9c9d
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-11 20:53:05 -0300

    Sequência na organização do projeto (Esqueleto)

---
+++ src/repositories/address_repository.py
@@ -0,0 +1,5 @@
+# TODO: create(address) -> Salva o endereço e retorna o ID final
+# TODO: get_by_id(id) -> Retorna o endereço pelo ID
+# TODO: get_all() -> Retorna lista com todos os endereços
+# TODO: update(address) -> Atualiza o endereço e retorna o ID final
+# TODO: delete(id) -> Deleta o endereço pelo ID
---
+++ src/repositories/customer_repository.py
@@ -0,0 +1,6 @@
+# TODO: create(customer) -> Salva o cliente e retorna o ID final
+# TODO: get_by_id(id) -> Retorna o cliente pelo ID
+# TODO: get_all() -> Retorna lista com todos os clientes
+# TODO: update(customer) -> Atualiza o cliente e retorna o ID final
+# TODO: delete(id) -> Deleta o cliente pelo ID
+

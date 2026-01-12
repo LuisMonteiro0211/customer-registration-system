4b0d674e749d2c1504f3d87392e0c5c68c5d3861
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-11 22:12:57 -0300

    Adicona a pasta service ao projeto

---
+++ src/services/customer_service.py
@@ -0,0 +1,6 @@
+# TODO: create_customer(dto) -> Salva o cliente e retorna o ID final
+# TODO: get_customer_by_id(id) -> Chama o repositório para buscar o cliente pelo ID
+# TODO: get_all_customers() -> Chama o repositório para buscar todos os clientes
+# TODO: update_customer(id, dto) -> Atualiza o cliente e retorna o ID final
+# TODO: delete_customer(id) -> Chama o repositório para deletar o cliente pelo ID
+

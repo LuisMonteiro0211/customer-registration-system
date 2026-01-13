ed2d5ea2f7d5340b4e83e18959abdd7899208d9c
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-12 22:24:41 -0300

    Cria estrtura para futuras queries

--- src/repositories/customer_repository.py
+++ src/repositories/customer_repository.py
@@ -1,6 +1,25 @@
-# TODO: create(customer) -> Salva o cliente e retorna o ID final
-# TODO: get_by_id(id) -> Retorna o cliente pelo ID
-# TODO: get_all() -> Retorna lista com todos os clientes
-# TODO: update(customer) -> Atualiza o cliente e retorna o ID final
-# TODO: delete(id) -> Deleta o cliente pelo ID
+from models.customer import Customer
+from typing import List
+class CustomerRepository:
+    def __init__(self) -> None:
+        pass
 
+    def create(self, customer: Customer) -> int:
+        # TODO: create(customer) -> Salva o cliente e retorna o ID final
+        pass
+
+    def get_by_id(self, id: int) -> Customer:
+        # TODO: get_by_id(id) -> Retorna o cliente pelo ID
+        pass
+
+    def get_all(self) -> List[Customer]:
+        # TODO: get_all() -> Retorna lista com todos os clientes
+        pass
+
+    def update(self, customer: Customer) -> int:
+        # TODO: update(customer) -> Atualiza o cliente e retorna o ID final
+        pass
+
+    def delete(self, id: int) -> None:
+        # TODO: delete(id) -> Deleta o cliente pelo ID
+        pass

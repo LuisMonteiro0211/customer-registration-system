4d7f98e957c2799387cd01a16a39dd5f7b73f686
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-14 23:35:34 -0300

    Adiciona função de conexão com o banco de dados

---
+++ src/database/connection.py
@@ -0,0 +1,25 @@
+from mysql.connector import connect, Error
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv(Path(".env")) # Carrega as variáveis de ambiente do arquivo .env
+
+def get_connection():
+    try:
+        connection = connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        port=int(os.getenv('DB_PORT'))
+        )   
+        return connection
+
+    except Error as error:
+        print(f"Erro na conexão com o banco de dados: {error}")
+        raise error # Re-lança o erro para ser tratado pelo chamador
+
+
+if __name__ == "__main__":
+    # Testa a conexão com o banco de dados
+    connection = get_connection()

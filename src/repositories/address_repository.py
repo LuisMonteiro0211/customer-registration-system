bc7b0dc6b39142fbfe80d4b028a2ebb89e02a52b
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-17 18:16:56 -0300

    Adiciona organização do arquivo de address_repository

--- src/repositories/address_repository.py
+++ src/repositories/address_repository.py
@@ -1,5 +1,28 @@
-# TODO: create(address) -> Salva o endereço e retorna o ID final
-# TODO: get_by_id(id) -> Retorna o endereço pelo ID
-# TODO: get_all() -> Retorna lista com todos os endereços
-# TODO: update(address) -> Atualiza o endereço e retorna o ID final
-# TODO: delete(id) -> Deleta o endereço pelo ID
+from src.models.address import Address
+from typing import List
+
+class AddressRepository:
+    def __init__(self) -> None:
+        # Como é uma classe apenas de execução de métodos não precisa de inicialização
+        # Tendo em vista que os dados já vem preparados para uso
+        pass
+    
+    def create(self, address: Address) -> int:
+        # TODO: create(address) -> Salva o endereço e retorna o ID final
+        pass
+
+    def get_by_id(self, id: int) -> Address:
+        # TODO: get_by_id(id) -> Retorna o endereço pelo ID
+        pass
+
+    def get_all(self) -> List[Address]:
+        # TODO: get_all() -> Retorna lista com todos os endereços
+        pass
+
+    def update(self, address: Address) -> int:
+        # TODO: update(address) -> Atualiza o endereço e retorna o ID final
+        pass
+
+    def delete(self, id: int) -> None:
+        # TODO: delete(id) -> Deleta o endereço pelo ID
+        pass

c6072be5508256368c6ac98b4a20b9543d160148
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-18 21:07:22 -0300

    Atualiza lista de pendências (TODO)

--- src/services/customer_service.py
+++ src/services/customer_service.py
@@ -1,4 +1,4 @@
-# TODO: create_customer(dto) -> Salva o cliente e retorna o ID final
+# TODO [X]: create_customer(dto) -> Salva o cliente e retorna o ID final
 # TODO: get_customer_by_id(id) -> Chama o repositório para buscar o cliente pelo ID
 # TODO: get_all_customers() -> Chama o repositório para buscar todos os clientes
 # TODO: update_customer(id, dto) -> Atualiza o cliente e retorna o ID final

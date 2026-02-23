a19dd97db4636237bc5d000a266065963d82c934
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-02-22 21:37:30 -0300

    Adicona teste ao service do customer

--- test/test_customer_service.py
+++ test/test_customer_service.py
@@ -2,79 +2,118 @@ import pytest
 from src.services.customer_service import CustomerService
 from src.dtos.customer_dto import CreateCustomerDTO
 from datetime import date
-
+from src.repositories.customer_repository import CustomerRepository
+from src.dtos.customer_dto import UpdateCustomerDTO
 class FakeCustomerRepository:
-    def create(self, customer):
+    # Criado para não precisar de conexão com o banco de dados
+    # Simula o método create do repositório
+    def create(self, customer) -> int:
         return 1
 
+    def update(self, customer) -> int:
+        return 1
 
-def test_create_customer_with_invalid_email():
-    repository = FakeCustomerRepository()
-    service = CustomerService(repository)
+    def delete(self, id: int) -> int:
+        return 1
 
-    dto_test = CreateCustomerDTO(
-        first_name="Luis",
-        last_name="Monteiro",
-        birth_date=date(2004, 11, 2),
-        email="luisfelipegmail.com",
+customer_service = CustomerService(FakeCustomerRepository())
+
+def test_create_customer_success():
+    dto_success = CreateCustomerDTO(
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva@example.com",
         phone="(11) 99999-9999"
     )
+    customer_created = customer_service.create_customer(dto_success)
+    assert customer_created > 0
 
-    with pytest.raises(ValueError):
-        service.create_customer(dto_test)
-
-def test_create_customer_with_invalid_phone():
-    repository = FakeCustomerRepository()
-    service = CustomerService(repository)
-
-    dto_test = CreateCustomerDTO(
-        first_name="Luis",
-        last_name="Monteiro",
-        birth_date=date(2004, 11, 2),
-        email="luisfelipe@gmail.com",
-        phone="119999999"
-    )
-    
-    with pytest.raises(ValueError):
-        service.create_customer(dto_test)
-
-def test_create_customer_with_invalid_birth_date():
-    repository = FakeCustomerRepository()
-    service = CustomerService(repository)
-
-    dto_test = CreateCustomerDTO(
-        first_name="Luis",
-        last_name="Monteiro",
-        birth_date=date(2030, 11, 2),
-        email="luisfelipe@gmail.com",
-        phone="(11) 99999-9999"
-    )
-    
-    with pytest.raises(ValueError):
-        service.create_customer(dto_test)
-
-def test_create_customer_with_invalid_first_name():
-    repository = FakeCustomerRepository()
-    service = CustomerService(repository)
-
-    dto_test = CreateCustomerDTO(
+def test_create_customer_invalid_name():
+    dto_invalid_name = CreateCustomerDTO(
         first_name="",
-        last_name="Monteiro",
-        birth_date=date(2004, 11, 2),
-        email="luisfelipe@gmail.com",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva@example.com",
         phone="(11) 99999-9999"
     )
     with pytest.raises(ValueError):
-        service.create_customer(dto_test)
+        customer_service.create_customer(dto_invalid_name)
 
-def test_create_customer_with_invalid_last_name():
-    repository = FakeCustomerRepository()
-    service = CustomerService(repository)
-
-    dto_test = CreateCustomerDTO(
-        first_name="Luis",
-        last_name="",
-        birth_date=date(2004, 11, 2),
-        email="luisfelipe@gmail.com",
+def test_create_customer_invalid_email():
+    dto_invalid_email = CreateCustomerDTO(
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva.example.com",
         phone="(11) 99999-9999"
-    )   
+    )
+    with pytest.raises(ValueError):
+        customer_service.create_customer(dto_invalid_email)
+
+def test_create_customer_invalid_phone():
+    dto_invalid_phone = CreateCustomerDTO(
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva@example.com",
+        phone="11999999999"
+    )
+    with pytest.raises(ValueError):
+        customer_service.create_customer(dto_invalid_phone)
+
+def test_create_customer_invalid_birth_date():
+    dto_invalid_birth_date = CreateCustomerDTO(
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1899, 1, 1),
+        email="joao.silva@example.com",
+        phone="(11) 99999-9999"
+    )
+    with pytest.raises(ValueError):
+        customer_service.create_customer(dto_invalid_birth_date)
+
+def test_create_customer_invalid_birth_date_future():
+    dto_invalid_birth_date_future = CreateCustomerDTO(
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(2026, 12, 31),
+        email="joao.silva@example.com",
+        phone="(11) 99999-9999"
+    )
+    with pytest.raises(ValueError):
+        customer_service.create_customer(dto_invalid_birth_date_future)
+
+def test_update_customer_success():
+    dto_success = UpdateCustomerDTO(
+        id=1,
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva@example.com",
+        phone="(11) 99999-9999"
+    )
+    customer_updated = customer_service.update_customer(dto_success)
+    assert customer_updated > 0
+
+def test_update_customer_invalid_id():
+    dto_invalid_id = UpdateCustomerDTO(
+        id=0,
+        first_name="João",
+        last_name="Silva",
+        birth_date=date(1990, 1, 1),
+        email="joao.silva@example.com",
+        phone="(11) 99999-9999"
+    )
+    with pytest.raises(ValueError):
+        customer_service.update_customer(dto_invalid_id)
+
+def test_delete_customer_success():
+    id = 1
+    deleted = customer_service.delete_customer(id)
+    assert deleted > 0
+
+def test_delete_customer_invalid_id():
+    id = 0
+    with pytest.raises(ValueError):
+        customer_service.delete_customer(id)
